4d204b1033bc0d601137f452ff5edf3d0e7f5616
Randy Konwalski <germankiller145@gmail.com>
2019-02-16 08:42:33 -0800

    added funcbutton

--- calcwindow.cpp
+++ calcwindow.cpp
@@ -1,12 +1,20 @@
 #include "calcwindow.h"
 #include "ui_calcwindow.h"
 
+bool mul = false;
+bool add = false;
+bool sub = false;
+bool divs = false;
+double lastval = 0.0;
+double curval = 0.0;
 
 CalcWindow::CalcWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::CalcWindow)
 {
     ui->setupUi(this);
+
+    ui->lineEdit->setText("0.0");
 
     connect(ui->Button_0, SIGNAL(released()), this, SLOT(NumPressed()));
     connect(ui->Button_1, SIGNAL(released()), this, SLOT(NumPressed()));
@@ -23,6 +31,7 @@ CalcWindow::CalcWindow(QWidget *parent) :
     connect(ui->Button_SUB, SIGNAL(released()), this, SLOT(FuncPressed()));
     connect(ui->Button_MUL, SIGNAL(released()), this, SLOT(FuncPressed()));
     connect(ui->Button_DIV, SIGNAL(released()), this, SLOT(FuncPressed()));
+    connect(ui->Button_EQ, SIGNAL(released()), this, SLOT(FuncPressed()));
 
     connect(ui->Button_CE, SIGNAL(released()), this, SLOT(CEPressed()));
     connect(ui->Button_C, SIGNAL(released()), this, SLOT(CPressed()));
@@ -47,6 +56,10 @@ void CalcWindow::NumPressed(){
 
 void CalcWindow::CEPressed(){
     ui->lineEdit->clear();
+    divs = false;
+    mul = false;
+    add = false;
+    sub = false;
 }
 
 void CalcWindow::CPressed(){
@@ -54,5 +67,39 @@ void CalcWindow::CPressed(){
 }
 
 void CalcWindow::FuncPressed(){
-
+    QPushButton *button = (QPushButton *)sender();
+    curval = ui->lineEdit->text().toDouble();
+    double newval = 0;
+    if(QString::compare(button->text(), "/", Qt::CaseSensitive) == 0){
+        divs = true;
+        lastval = curval;
+        ui->lineEdit->clear();
+    } else if(QString::compare(button->text(), "*", Qt::CaseInsensitive) == 0){
+        mul = true;
+        lastval = curval;
+        ui->lineEdit->clear();
+    } else if(QString::compare(button->text(), "+", Qt::CaseInsensitive) == 0){
+        add = true;
+        lastval = curval;
+        ui->lineEdit->clear();
+    } else if(QString::compare(button->text(), "-", Qt::CaseInsensitive) == 0){
+        sub = true;
+        lastval = curval;
+        ui->lineEdit->clear();
+    } else if(QString::compare(button->text(), "=", Qt::CaseInsensitive) == 0){
+        if(divs == true){
+            newval = lastval / curval;
+            ui->lineEdit->setText(QString::number(newval));
+        } else if(mul == true){
+            newval = lastval * curval;
+            ui->lineEdit->setText(QString::number(newval));
+        } else if(add == true){
+            newval = lastval + curval;
+            ui->lineEdit->setText(QString::number(newval));
+        } else if(sub == true){
+            newval = lastval - curval;
+            ui->lineEdit->setText(QString::number(newval));
+        } else {
+        }
+    }
 }
